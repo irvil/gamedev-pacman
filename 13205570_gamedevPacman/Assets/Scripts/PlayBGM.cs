eac8cdb3b5ad605c5cbfbb37bd857b48b902b5e4
irvil <ivn55villanueva@gmail.com>
2020-09-21 14:47:37 +1000

    Added BGM and player sounds

---
+++ 13205570_gamedevPacman/Assets/Scripts/PlayBGM.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBGM : MonoBehaviour
+{
+    public AudioSource source; // Will be self
+
+    public AudioClip introClip;
+    // Clips to loop
+    public AudioClip ghostNormalClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.loop = true;
+        StartCoroutine(playStartingAudio());
+    }
+
+    // Update is called once per frame
+    IEnumerator playStartingAudio()
+    {
+        source.clip = introClip;
+        source.Play();
+        yield return new WaitForSeconds(source.clip.length);
+        source.clip = ghostNormalClip;
+        source.Play();
+    }
+}
