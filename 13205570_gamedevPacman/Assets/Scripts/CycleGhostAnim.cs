071e1fa5195b199c7bcd7bee06a9047c595c3871
irvil <ivn55villanueva@gmail.com>
2020-09-21 15:47:24 +1000

    Started script to attempt to cycle through ghost animations

---
+++ 13205570_gamedevPacman/Assets/Scripts/CycleGhostAnim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleGhostAnim : MonoBehaviour
+{
+    private int lastTime;
+    private int lastScared;
+    private int lastRecover;
+    private int lastDead;
+    private float timer;
+
+    // Time increments
+    float scaredWait = 3.0f;
+    float recoverWait = 6.0f;
+    float deadWait = 12.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (lastTime < timer) lastTime++;
+
+        if (lastTime % (int) scaredWait == 0){
+            //GhostAnimator_Red.SetTrigger("scared");
+        }
+    }
+}
